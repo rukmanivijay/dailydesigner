314edd5197e064adf128408cb5a7bfbaa3cf8cee
Rukmani Vijayakumar <rukmani.vijayakumar@gmail.com>
2014-07-02 19:00:24 -0400

    First commit

---
+++ db/migrate/20140613004440_create_dailydesignerapps.rb
@@ -0,0 +1,12 @@
+class CreateDailydesignerapps < ActiveRecord::Migration
+  def change
+    create_table :dailydesignerapps do |t|
+      t.string :name
+      t.string :sex
+      t.string :state
+      t.string :city
+
+      t.timestamps
+    end
+  end
+end
